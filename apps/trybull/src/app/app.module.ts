6f433bd57ae2aaf099d4442d3f24b3e9d85150f2
codev911 <codev911@gmail.com>
2023-12-07 16:25:55 +0700

    add bull module

--- apps/trybull/src/app/app.module.ts
+++ apps/trybull/src/app/app.module.ts
@@ -1,12 +1,21 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { BullModule } from '@nestjs/bull';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
   imports: [
-    ConfigModule.forRoot()
+    ConfigModule.forRoot(),
+    BullModule.forRoot({
+      redis: {
+        host: process.env.REDIS_HOST,
+        port: parseInt(process.env.REDIS_PORT),
+        password: process.env.REDIS_PASSWORD,
+      },
+    }),
+
   ],
   controllers: [AppController],
   providers: [AppService],
