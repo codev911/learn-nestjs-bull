ed8fd617a70f0953726f68c110b132077a38e4aa
codev911 <codev911@gmail.com>
2023-12-07 21:58:46 +0700

    add backup controller

---
+++ apps/trybull/src/app/bull/bull.controller-old.ts
@@ -0,0 +1,66 @@
+import { InjectQueue } from '@nestjs/bull';
+import { Controller, Post } from '@nestjs/common';
+import { Queue } from 'bull';
+
+@Controller('bull')
+export class BullController {
+  constructor(@InjectQueue('blockchain') private readonly blockchainQueue: Queue) {}
+
+  @Post('multicall')
+  async transcode() {
+    const queue = [];
+
+    for (let index = 0; index < 5; index++) {
+      queue.push(
+        this.blockchainQueue.add(
+          'faucet',
+          {
+            wallets: [
+              '0xE018554a4FdC054e3f57d90e2d764a86A4486B79',
+              '0xE018554a4FdC054e3f57d90e2d764a86A4486B79'
+            ],
+            amount: 0.01
+          },
+          {
+            delay: 2000
+          }
+        )
+      );
+
+      queue.push(
+        this.blockchainQueue.add(
+          'multicall',
+          [
+            {
+              from: '0xE018554a4FdC054e3f57d90e2d764a86A4486B79',
+              to: '0xE018554a4FdC054e3f57d90e2d764a86A4486B79',
+              id: 0,
+              amount: 1
+            },
+            {
+              from: '0xE018554a4FdC054e3f57d90e2d764a86A4486B79',
+              to: '0xE018554a4FdC054e3f57d90e2d764a86A4486B79',
+              id: 0,
+              amount: 1
+            },
+            {
+              from: '0xE018554a4FdC054e3f57d90e2d764a86A4486B79',
+              to: '0xE018554a4FdC054e3f57d90e2d764a86A4486B79',
+              id: 0,
+              amount: 1
+            },
+          ],
+          {
+            delay: 2000
+          }
+        )
+      )
+    }
+
+    await Promise.all(queue);
+
+    return {
+      process: true
+    }
+  }
+}
